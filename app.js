2cf9b5ddd6d1d6a979fe5d45516ea93ec794c53e
unknown <yeejiac@gmail.com>
2021-05-06 23:31:31 +0800

    add html template

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-var http = require('http');
+let express = require('express');
+  //載入express模組
+let engine = require('ejs-locals');
+  //載入ejs-locals 模組
+let app = express();
+  // 使用express
+app.engine('ejs', engine);
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('index', {'title': '首頁',});
+})
+
+port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
